9636080a095674c0016458a895baa4415c2b45c3
Samuel <ladaposamuel@outlook.com>
2023-06-19 22:53:51 +0100

    (chore) redirect cleanup

--- includes/_login_check.php
+++ includes/_login_check.php
@@ -4,12 +4,12 @@
     $basename = substr(strtolower(basename($_SERVER['PHP_SELF'])),0,strlen(basename($_SERVER['PHP_SELF']))-4);;
 
     if(!isset($_SESSION['user']) && $basename != "index"){
-        header("Location: ". Index_Page_Link);
+        header("Location: ". INDEX_LINK);
     }
 
     if($basename == "index") {
         if(!empty($_SESSION['user']))
-            header("Location:".Gallery_Page_Link);
+            header("Location:". INDEX_LINK);
     }
 
 ?>
